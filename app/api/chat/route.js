07b899f26b617a7ecf1c967845bffa20c84e2ebc
MuhammadUsmanGM <MuhammadUsmanGM@users.noreply.github.com>
2025-12-10 02:56:50 +0000

    fix : updated model

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -19,7 +19,7 @@ export async function POST(req) {
 
     // Prepare the request to the Gemini API
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
         method: 'POST',
         headers: {
